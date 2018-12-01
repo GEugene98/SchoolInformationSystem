67f8868e941bdcb5954530a07aaadc66c2cafc9f
egavrishin <egavrishin@gmcs.ru>
2018-12-01 13:54:30 +0300

    monitoring models

--- WorkScheduler/Context.cs
+++ WorkScheduler/Context.cs
@@ -2,11 +2,13 @@
 using Microsoft.EntityFrameworkCore;
 using WorkScheduler.Models;
 using WorkScheduler.Models.Identity;
+using WorkScheduler.Models.Monitoring;
 
 namespace WorkScheduler
 {
     public class Context : IdentityDbContext<User>
     {
+        //Scheduler
         public DbSet<AcademicYear> AcademicYears { get; set; }
         public DbSet<Action> Actions { get; set; }
         public DbSet<ActionUser> ActionUsers { get; set; }
@@ -14,6 +16,22 @@ namespace WorkScheduler
         public DbSet<ConfirmationForm> ConfirmationForms { get; set; }
         public DbSet<WorkSchedule> WorkSchedules { get; set; }
         public DbSet<Ticket> Tickets { get; set; }
+
+        //Monitoring
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Certificate> Certificates { get; set; }
+        public DbSet<Diploma> Diplomas { get; set; }
+        public DbSet<Experience> Experiences { get; set; }
+        public DbSet<Passport> Passports { get; set; }
+        public DbSet<RefCourse> RefCourses { get; set; }
+        public DbSet<Reward> Rewards { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<StudentAchivment> StudentAchivments { get; set; }
+        public DbSet<Vacation> Vacations { get; set; }
+        public DbSet<Worker> Workers { get; set; }
+        public DbSet<WorkerAchivment> WorkerAchivments { get; set; }
+        public DbSet<WorkHistory> WorkHistory { get; set; }
+        public DbSet<WorkPeriod> WorkPeriods { get; set; }
 
         public Context(DbContextOptions<Context> options) : base(options)
         {
