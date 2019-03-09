57210f21d6648d124b532df8f343f346ae8486ec
egavrishin <egavrishin@gmcs.ru>
2019-03-09 22:47:25 +0300

    part of monitoring

--- WorkScheduler/Services/Monitoring/DictionaryService.cs
+++ WorkScheduler/Services/Monitoring/DictionaryService.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using WorkScheduler.Models.Monitoring.Shared;
@@ -16,10 +17,82 @@ namespace WorkScheduler.Services.Monitoring
             Db = context;
         }
 
-        public IEnumerable<Student> GetStudents()
+        public ClassVievModel GetClass(int studentId, int? academicYearId = null)
         {
-            return Db.Students.Where(s => !s.IsDeleted).ToList();
+            if (academicYearId == null)
+            {
+                var academicYear = Db.AcademicYears.FirstOrDefault(a => a.Start >= DateTime.Now && DateTime.Now <= a.End);
+
+                if (academicYear == null)
+                {
+                    throw new Exception("There's no active academic years");
+                }
+                else
+                {
+                    academicYearId = academicYear.Id;
+                }
+            }
+
+            var studentsQuery = Db.Students.Where(s => s.Id == studentId);
+            var classStudentQuery = Db.ClassStudents;
+            var classQuery =
+                Db.Classes
+                .Include(c => c.AcademicYear)
+                .Where(c => c.AcademicYearId == academicYearId);
+
+            var foundClass =
+                (from s in studentsQuery
+                 join cs in classStudentQuery on s.Id equals cs.StudentId
+                 join c in classQuery on cs.ClassId equals c.Id
+                 select new { Student = s, Class = c })
+                .FirstOrDefault()?.Class;
+
+            if (foundClass == null)
+            {
+                return null;
+            }
+
+            var classModel = new ClassVievModel
+            {
+                Id = foundClass.Id,
+                Name = foundClass.Name,
+                AcademicYear = new DictionaryViewModel<int>
+                {
+                    Id = foundClass.AcademicYear.Id,
+                    Name = foundClass.AcademicYear.Name
+                }
+            };
+
+            return classModel;
         }
+
+        //public IEnumerable<StudentViewModel> GetStudents()
+        //{
+        //    var studentsQuery = Db.Students;
+        //    var classStudentQuery = Db.ClassStudents;
+        //    var classQuery =
+        //        Db.Classes
+        //        .Include(c => c.AcademicYear)
+        //        .Where(c => c.AcademicYearId == academicYearId);
+
+        //    var joinResult =
+        //        from s in studentsQuery
+        //        join cs in classStudentQuery on s.Id equals cs.StudentId
+        //        join c in classQuery on cs.ClassId equals c.Id
+        //        select new { Student = s, Class = c };
+
+        //    return Db.Students.Where(s => !s.IsDeleted)
+        //        .ToList()
+        //        .Select(s => new StudentViewModel
+        //        {
+        //            Id = s.Id,
+        //            Class = new ClassVievModel
+        //            {
+
+        //            } 
+        //        })
+        //        ;
+        //}
 
         public IEnumerable<ClassVievModel> GetStudentsByClasses(int academicYearId)
         {
