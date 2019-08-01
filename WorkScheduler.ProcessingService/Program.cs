39871ba557059a27aa1e1035eda52d32c3610162
geugene98 <geugene1998@gmail.com>
2019-08-01 15:13:30 +0300

    processing service

---
+++ WorkScheduler.ProcessingService/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WorkScheduler.ProcessingService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
