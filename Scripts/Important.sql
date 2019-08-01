efde4262a461c377fa10d88c43f2f8b263cfaf76
geugene98 <geugene1998@gmail.com>
2019-08-01 15:11:12 +0300

    garbage

--- Scripts/Important.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE "Tickets" DROP COLUMN "End";
-
-ALTER TABLE "Tickets" DROP COLUMN "Start";
-
-ALTER TABLE "Tickets" ADD "Important" boolean NOT NULL DEFAULT FALSE;
-
-INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
-VALUES ('20190206212307_Important', '2.1.3-rtm-32065');
-
