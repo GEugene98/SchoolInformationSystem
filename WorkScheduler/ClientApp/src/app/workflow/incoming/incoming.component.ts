9d56dfe54b03546fc3eac223db1147e78651e372
Евгений Гавришин <geugene1998@ya.ru>
2021-09-12 17:52:03 +0300

    cut description

--- WorkScheduler/ClientApp/src/app/workflow/incoming/incoming.component.ts
+++ WorkScheduler/ClientApp/src/app/workflow/incoming/incoming.component.ts
@@ -105,6 +105,13 @@ export class IncomingComponent implements OnInit {
     this.modalRef.hide();
   }
 
+  getCutComment(comment: string) {
+    if (comment.length < 150) {
+      return comment+'';
+    }
+    return comment.substring(0, 150) + "...";
+  }
+
   uploadEventHandler(e: UploadEvent) {
     e.headers = e.headers.append('transaction-id', this.transactionId);
   }
